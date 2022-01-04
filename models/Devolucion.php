bbd4a4dddb1bb0091007b3d343e22214a59d96d4
Eric Morales <ericdaniel3103@gmail.com>
2022-01-04 11:49:39 -0600

    Se elimino columna estado de Devolucion.php

--- models/Devolucion.php
+++ models/Devolucion.php
@@ -6,12 +6,11 @@ class Devolucion extends ActiveRecord
 {
     // Base de datos
     protected static $tabla = 'devoluciones';
-    protected static $columnasDB = ['id', 'fecha', 'hora', 'estado', 'apartadoId'];
+    protected static $columnasDB = ['id', 'fecha', 'hora', 'apartadoId'];
 
     public $id;
     public $fecha;
     public $hora;
-    public $estado;
     public $apartadoId;
 
     public function __construct($args = [])
@@ -19,7 +18,6 @@ class Devolucion extends ActiveRecord
         $this->id = $args['id'] ?? null;
         $this->fecha = $args['fecha'] ?? '';
         $this->hora = $args['hora'] ?? '';
-        $this->estado = $args['estado'] ?? '0';
         $this->apartadoId = $args['apartadoId'] ?? '';
     }
 }
