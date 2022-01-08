c377bba3b003cd2edac1d957d3ccd0acef382cb8
Eric Morales <ericdaniel3103@gmail.com>
2022-01-08 12:45:43 -0600

    Removiendo descripcion del modelo componente

--- models/Componente.php
+++ models/Componente.php
@@ -6,7 +6,7 @@ class Componente extends ActiveRecord
 {
     // Base de datos
     protected static $tabla = 'componentes';
-    protected static $columnasDB = ['id', 'nombre', 'categoria', 'descripcion', 'estado'];
+    protected static $columnasDB = ['id', 'nombre', 'categoria', 'estado'];
 
     public $id;
     public $nombre;
@@ -19,23 +19,16 @@ class Componente extends ActiveRecord
         $this->id = $args['id'] ?? null;
         $this->nombre = $args['nombre'] ?? '';
         $this->categoria = $args['categoria'] ?? '';
-        $this->descripcion = $args['descripcion'] ?? '';
         $this->estado = $args['estado'] ?? '0';
     }
 
     public function validar()
     {
-        if (!is_numeric($this->id)) {
-            self::$alertas['error'][] = 'Debes Agregar un Folio Válido';
-        }
         if (!$this->nombre) {
             self::$alertas['error'][] = 'El Nombre del Componente es Obligatorio';
         }
         if (!$this->categoria) {
             self::$alertas['error'][] = 'La Categoría del Componente es Obligatorio';
-        }
-        if (!$this->descripcion) {
-            self::$alertas['error'][] = 'La Descripcion del Componente es Obligatorio';
         }
 
         return self::$alertas;
--- views/componentes/formulario.php
+++ views/componentes/formulario.php
@@ -1,24 +1,14 @@
-    <div class="campo">
-        <label for="id">Folio</label>
-        <input type="text" id="id" placeholder="Folio" name="id" value="<?php echo $componente->id; ?>">
-    </div>
+<div class="campo">
+    <label for="nombre">Nombre</label>
+    <input type="text" id="nombre" placeholder="Nombre Componente" name="nombre" value="<?php echo $componente->nombre; ?>">
+</div>
 
-    <div class="campo">
-        <label for="nombre">Nombre</label>
-        <input type="text" id="nombre" placeholder="Nombre Servicio" name="nombre" value="<?php echo $componente->nombre; ?>">
-    </div>
-
-    <div class="campo">
-        <label for="descripcion">Descripcion</label>
-        <input type="text" id="descripcion" placeholder="Descripcion" name="descripcion" value="<?php echo $componente->descripcion; ?>">
-    </div>
-
-    <div class="campo">
-        <label for="categoria">Categoria</label>
-        <select name="categoria" id="categoria">
-            <?php
-            foreach ($categorias as $categoria) { ?>
-                <option value="<?php echo $categoria->nombre ?>"><?php echo $categoria->nombre ?></option>
-            <?php } ?>
-        </select>
-    </div>
+<div class="campo">
+    <label for="categoria">Categoria</label>
+    <select name="categoria" id="categoria">
+        <?php
+        foreach ($categorias as $categoria) { ?>
+            <option value="<?php echo $categoria->nombre ?>"><?php echo $categoria->nombre ?></option>
+        <?php } ?>
+    </select>
+</div>
